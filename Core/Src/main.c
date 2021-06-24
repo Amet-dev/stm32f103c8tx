73cc7bec53cf787bee03f23912986a1ef90ca735
amet-dev <>
2021-06-24 11:35:49 +0300

    config_modbus_gpio_work
    config_modbus_gpio_work

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,7 +63,7 @@ const osThreadAttr_t myTaskGpio_attributes = {
 };
 /* USER CODE BEGIN PV */
 modbusHandler_t ModbusH;
-uint16_t ModbusDATA[8];
+uint16_t ModbusDATA[9];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -118,7 +118,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
   ModbusH.uiModbusType = SLAVE_RTU;
   ModbusH.port =  &huart1;
-  ModbusH.u8id = 01; //Modbus slave ID
+  ModbusH.u8id = 02; //Modbus slave ID
   ModbusH.u16timeOut = 1000;
   ModbusH.EN_Port = NULL;
   ModbusH.u32overTime = 0;
@@ -128,6 +128,10 @@ int main(void)
   ModbusInit(&ModbusH);
   //Start capturing traffic on serial Port
   ModbusStart(&ModbusH);
+
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13,GPIO_PIN_RESET);
+  HAL_Delay(2000);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13,GPIO_PIN_SET);
   /* USER CODE END 2 */
 
   /* Init scheduler */
@@ -387,7 +391,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(DO7_GPIO_Port, DO7_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(DO1_GPIO_Port, DO1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, DO1_Pin|DO2_Pin|DO3_Pin|DO6_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : LED_Pin */
   GPIO_InitStruct.Pin = LED_Pin;
@@ -417,12 +421,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(DO7_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : DO1_Pin */
-  GPIO_InitStruct.Pin = DO1_Pin;
+  /*Configure GPIO pins : DO1_Pin DO2_Pin DO3_Pin DO6_Pin */
+  GPIO_InitStruct.Pin = DO1_Pin|DO2_Pin|DO3_Pin|DO6_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(DO1_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
@@ -461,10 +465,62 @@ void StartTask02(void *argument)
   /* Infinite loop */
   for(;;)
   {
+	  //если поступил запрос с modbus
+	  if ( ModbusDATA[0] != 0 ) {
+		  // проверка введенной команды и установка пинов
+	  switch (ModbusDATA[0])
+		  {
+		  case 1: HAL_GPIO_WritePin(DO1_GPIO_Port, DO1_Pin, GPIO_PIN_SET);
+				  break;
+		  case 2: HAL_GPIO_WritePin(DO2_GPIO_Port, DO2_Pin, GPIO_PIN_SET);
+				  break;
+		  case 3: HAL_GPIO_WritePin(DO3_GPIO_Port, DO3_Pin, GPIO_PIN_SET);
+				  break;
+		  case 4: //HAL_GPIO_WritePin(DO6_GPIO_Port, DO4_Pin, GPIO_PIN_SET);
+				  break;
+		  case 5: //HAL_GPIO_WritePin(DO6_GPIO_Port, DO5_Pin, GPIO_PIN_SET);
+				  break;
+		  case 6: HAL_GPIO_WritePin(DO6_GPIO_Port, DO6_Pin, GPIO_PIN_SET);
+				  break;
+		  case 7: HAL_GPIO_WritePin(DO7_GPIO_Port, DO7_Pin, GPIO_PIN_SET);
+				  break;
+		  case 111:
+				  HAL_GPIO_WritePin(DO1_GPIO_Port, DO1_Pin, GPIO_PIN_RESET);
+				  HAL_GPIO_WritePin(DO2_GPIO_Port, DO2_Pin, GPIO_PIN_RESET);
+				  HAL_GPIO_WritePin(DO3_GPIO_Port, DO3_Pin, GPIO_PIN_RESET);
+				  //
+				  HAL_GPIO_WritePin(DO6_GPIO_Port, DO6_Pin, GPIO_PIN_RESET);
+				  HAL_GPIO_WritePin(DO7_GPIO_Port, DO7_Pin, GPIO_PIN_RESET);
+				  break;
+
+		  }
+		  ModbusDATA[0]=0;
+		  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+	  }
+
+	//установка регистров в соответствии с состоянием пинов
+	ModbusDATA[1]=					 HAL_GPIO_ReadPin(DO1_GPIO_Port, DO1_Pin);
+	ModbusDATA[1]=(ModbusDATA[1]<<1)+HAL_GPIO_ReadPin(DO2_GPIO_Port, DO2_Pin);
+	ModbusDATA[1]=(ModbusDATA[1]<<1)+HAL_GPIO_ReadPin(DO3_GPIO_Port, DO3_Pin);
+	ModbusDATA[1]=(ModbusDATA[1]<<1);
+	ModbusDATA[1]=(ModbusDATA[1]<<1);
+	ModbusDATA[1]=(ModbusDATA[1]<<1)+HAL_GPIO_ReadPin(DO6_GPIO_Port, DO6_Pin);
+	ModbusDATA[1]=(ModbusDATA[1]<<1)+HAL_GPIO_ReadPin(DO7_GPIO_Port, DO7_Pin);
+
+	ModbusDATA[2]=					 HAL_GPIO_ReadPin(DI1_GPIO_Port, DI1_Pin);
+	ModbusDATA[2]=(ModbusDATA[2]<<1)+HAL_GPIO_ReadPin(DI2_GPIO_Port, DI2_Pin);
+	ModbusDATA[2]=(ModbusDATA[2]<<1)+HAL_GPIO_ReadPin(DI3_GPIO_Port, DI3_Pin);
+	ModbusDATA[2]=(ModbusDATA[2]<<1)+HAL_GPIO_ReadPin(DI4_GPIO_Port, DI4_Pin);
+	ModbusDATA[2]=(ModbusDATA[2]<<1)+HAL_GPIO_ReadPin(DI5_GPIO_Port, DI5_Pin);
+	ModbusDATA[2]=(ModbusDATA[2]<<1)+HAL_GPIO_ReadPin(DI6_GPIO_Port, DI6_Pin);
+	ModbusDATA[2]=(ModbusDATA[2]<<1)+HAL_GPIO_ReadPin(DI7_GPIO_Port, DI7_Pin);
+
+	osDelay(1);
+	/*
     xSemaphoreTake(ModbusH.ModBusSphrHandle , 100);
 	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, ModbusH.au16regs[0] & 0x1);
 	xSemaphoreGive(ModbusH.ModBusSphrHandle);
-    osDelay(200);
+    osDelay(200);*/
   }
   /* USER CODE END StartTask02 */
 }
